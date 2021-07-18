9e62aad8911414f04765e5f88dd46def95304236
bstee615 <benjis@iastate.edu>
2021-07-18 19:08:11 -0400

    Add conditions to targets for insert noop

--- refactorings/insert_noop.py
+++ refactorings/insert_noop.py
@@ -1,13 +1,25 @@
 """Insert Noop: insert a statement that doesn't affect any other variables."""
 
 from pathlib import Path
+import networkx as nx
 import cpg
 
 
 def insert_noop(c_file, picker=lambda i: i[0], info=None):
     g = cpg.parse(Path(info["project"]), Path(c_file))
-    all_targets = [d["location"] for n, d in g.nodes.items(
-    ) if d["type"] == 'ExpressionStatement' and isinstance(d["location"], str)]
+    ast = g.edge_subgraph([e for e, d in g.edges.items() if d["type"] == 'IS_AST_PARENT']).copy()
+    node_type = nx.get_node_attributes(g, 'type')
+    node_location = nx.get_node_attributes(g, 'location')
+    def is_target(n):
+        is_valid_stmt = node_type[n] in ('ExpressionStatement', 'IfStatement', 'ElseStatement', 'ForStatement', 'WhileStatement')
+        has_valid_location = isinstance(node_location[n], str)
+        pred = list(ast.predecessors(n))
+        if len(pred) > 0:
+            parent_is_compound = node_type[pred[0]] == 'CompoundStatement'
+        else:
+            parent_is_compound = False
+        return is_valid_stmt and parent_is_compound and has_valid_location
+    all_targets = [node_location[n] for n in filter(is_target, ast.nodes)]
     if len(all_targets) == 0:
         return None
     location = picker(all_targets)
