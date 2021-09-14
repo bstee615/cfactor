623174cb84195605db1c4eed832727a46ba9e611
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2021-09-13 21:26:03 -0500

    Refactor TransformationProject to be agnostic of transformation style

--- refactorings/project.py
+++ refactorings/project.py
@@ -18,15 +18,26 @@ all_refactorings = [
 class TransformationProject:
     logger = logger
 
-    def __init__(self, c_filename, c_code, transforms=None, picker=first_picker, avoid=None):
+    def __init__(self, c_filename, c_code, transforms=None, picker=first_picker, avoid=None, style='one_of_each', style_args=None):
         if transforms is None:
             transforms = all_refactorings
         self.transforms = copy.deepcopy(transforms)
+        assert len(self.transforms) > 0, 'empty transform list!'
         self.picker = picker
 
         self.c_filename = c_filename
         self.c_code = c_code
         self.avoid = avoid
+        self.style = style
+        self.style_args = style_args
+
+    def get_transform(self):
+        if self.style == 'one_of_each':
+            if len(self.transforms) == 0:
+                self.logger.info('[%s] ran out of transforms', self.c_filename)
+                return None
+            else:
+                return self.transforms.pop(0)
 
     def apply_all(self, return_applied=False):
         """Do C source-to-source translation"""
@@ -34,33 +45,26 @@ class TransformationProject:
         transformations_applied = []
 
         # Apply all transforms one at a time
-        assert len(self.transforms) > 0, 'empty transform list!'
-        while len(self.transforms) > 0:
-            if len(self.transforms) == 0:
-                self.logger.info('[%s] ran out of transforms', self.c_filename)
-                break
-            else:
-                t = self.transforms[0]
+        t = self.get_transform()
+        while t is not None:
 
             try:
                 new_lines = t(self.c_filename, self.c_code, picker=self.picker, avoid_lines=self.avoid).run()
             except Exception as e:
                 self.logger.exception('[%s] exception while applying %s', self.c_filename, t.__name__, exc_info=e)
-                self.transforms.remove(t)
                 break
 
             # If it could not be applied, skip this transformation.
             # Most commonly means the transformation had no slot.
             if new_lines is None:
                 self.logger.debug('[%s] could not apply %s', self.c_filename, t.__name__)
-                self.transforms.remove(t)
-                continue
             else:
                 # Successfully applied the transformation.
-                self.transforms.remove(t)
                 transformations_applied.append(t)
                 self.logger.debug('[%s] applied %s', self.c_filename, t.__name__)
                 self.c_code = ''.join(new_lines)
+
+            t = self.get_transform()
         if return_applied:
             return self.c_code.splitlines(keepends=True), transformations_applied
         else:
